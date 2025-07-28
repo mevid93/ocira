c4f6689b374d900eec0de827389ae1ecce537aa4
Martin Vidjeskog <martin.vidjeskog@hotmail.com>
2025-07-28 16:45:47 +0300

    ...

---
+++ App/src/main.cpp
@@ -0,0 +1,49 @@
+#include <SFML/Graphics.hpp>
+#include <SFML/Window/VideoMode.hpp>
+
+// Function prototypes
+void centerTheWindow(sf::RenderWindow& window);
+
+/**
+ * @brief SDCCA main program.
+ *
+ * @return Returns 0 when execution completes succesfully.
+ */
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode({1920, 1080}), "SDCCA 0.1.0 - 20250728");
+    window.setFramerateLimit(30);
+    centerTheWindow(window);
+
+    while (window.isOpen())
+    {
+        while (const std::optional event = window.pollEvent())
+        {
+            if (event->is<sf::Event::Closed>())
+                window.close();
+        }
+
+        window.clear();
+
+        window.display();
+    }
+}
+
+/**
+ * @brief Takes sf::RenderWindow as input and centers it on the screen.
+ */
+void centerTheWindow(sf::RenderWindow& window)
+{
+    // Get desktop video mode (screen resolution)
+    sf::VideoMode desktopMode = sf::VideoMode::getDesktopMode();
+
+    // Calculate the center position of the screen
+    sf::Vector2i screenCenter(desktopMode.size.x / 2, desktopMode.size.y / 2);
+
+    // Calculate the top-left corner position of the window to center it
+    sf::Vector2i windowPosition(screenCenter.x - window.getSize().x / 2,
+                                screenCenter.y - window.getSize().y / 2);
+
+    // Set the window's position
+    window.setPosition(windowPosition);
+}
---
+++ Core/src/component.cpp
@@ -0,0 +1,17 @@
+#ifndef SDCCA_CORE_COMPONENT_H
+#define SDCCA_CORE_COMPONENT_H
+
+namespace SDCCA::Core
+{
+    enum class ComponentType
+    {
+        
+
+    }
+
+    class Component
+    {
+    };
+}
+
+#endif
